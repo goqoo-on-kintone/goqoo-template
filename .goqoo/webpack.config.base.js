4726013dfa789dc92369cef3d43c6fd31ca03a87
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-02-28 20:57:07 +0900

    fix: Dropbox連携機能は廃止

--- .goqoo/webpack.config.base.js
+++ .goqoo/webpack.config.base.js
@@ -3,48 +3,19 @@ const webpack = require('webpack')
 const path = require('path')
 const { rcFile } = require('rc-config-loader')
 require('dotenv').config()
-const DropboxKintone = require('./dropbox')
 const S3Plugin = require('webpack-s3-plugin')
 
 const { npm_package_name: projectName } = process.env
-const { apps, useDropbox } = rcFile('goqoo', { configFileName: path.resolve('config', 'goqoo.config') }).config
+const { apps } = rcFile('goqoo', { configFileName: path.resolve('config', 'goqoo.config') }).config
 
 const entry = apps.reduce((obj, appName) => {
   obj[appName] = ['babel-polyfill', path.resolve('apps', appName)]
   return obj
 }, {})
 
-const output = { path: path.resolve('dist') }
-if (useDropbox) {
-  const dropbox = {
-    rootDir: process.env.DROPBOX_ROOT,
-    subDir: process.env.DROPBOX_KINTONE_DIR,
-    token: process.env.DROPBOX_TOKEN,
-  }
-
-  if (!dropbox || !dropbox.rootDir) {
-    console.error('DROPBOX_ROOT: environment variable not found!')
-    process.exit(1)
-  }
-
-  const outputDir = `${dropbox.subDir || ''}/${projectName}`
-  output.path = path.resolve(path.join(dropbox.rootDir, outputDir))
-
-  if (dropbox.token) {
-    const dbxKintone = new DropboxKintone({
-      accessToken: dropbox.token,
-      localRootDir: dropbox.rootDir,
-    })
-    const dbxOutputPaths = apps.map((appName) => `/${outputDir}/${appName}.js`)
-    dbxKintone.fetchSharedLinks(dbxOutputPaths).then((paths) => {
-      console.log(JSON.stringify(paths, null, '  '))
-    })
-  }
-}
-
 const config = {
   entry,
-  output,
+  output: { path: path.resolve('dist') },
   devtool: 'source-map',
   module: {
     rules: [
