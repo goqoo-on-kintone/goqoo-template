36b501297ea61e5a25cb8dae8c190854eb04eb83
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-08-08 04:10:14 +0900

    wip: apps, environmentsあたりを検討中

--- templates/_new/goqoo.config.js
+++ templates/_new/goqoo.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   bundlerType: 'standard', // 'standard', 'vue', 'react'
+  environments: [
+    // TODO: dts-gen, ginue, プログラム内で全て共通化
+    {
+      name: 'development',
+      host: '<開発ドメイン>.cybozu.com',
+      apps: {
+        AppName1: 123,
+        AppName2: 246,
+      },
+    },
+    {
+      name: '<本番ドメイン>.cybozu.com',
+      apps: {
+        AppName1: 123,
+        AppName2: 246,
+      },
+    },
+  ],
+  // TODO: goqoo new時にginuercを一緒に作成（ginue.config.js）
+
+  // TODO: import時のrootをどこに置くか？特に何もしなくて良いか？
 }
---
+++ templates/_new/src/env.ts
@@ -0,0 +1,4 @@
+import { environments } from '../goqoo.config'
+// TODO: environmentsからappを取り出す
+// const spaces = environments[location.hostname]
+// export const app = spaces.find((s) => Object.values(s).some((appId) => appId === kintone.app.getId()))
