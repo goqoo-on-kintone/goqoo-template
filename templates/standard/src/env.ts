7f927961a0268a9659ab102070e9e7b1d333d739
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-08-10 00:50:30 +0900

    fix: importエラーになるのを一度コメントアウト

--- templates/standard/src/env.ts
+++ templates/standard/src/env.ts
@@ -1,4 +1,5 @@
-import { environments } from '../goqoo.config'
+// import { environments } from '../goqoo.config'
 // TODO: environmentsからappを取り出す
 // const spaces = environments[location.hostname]
 // export const app = spaces.find((s) => Object.values(s).some((appId) => appId === kintone.app.getId()))
+console.log()
