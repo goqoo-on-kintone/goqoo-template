d25d92fd2eec1a9eaae8fa3232d116b6fc2504b7
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-08-11 18:15:53 +0900

    feat: dts-gen用の設定追加、単語区切りのアプリ名作成

--- templates/standard/goqoo.config.js
+++ templates/standard/goqoo.config.js
@@ -1,23 +1,26 @@
 module.exports = {
-  bundlerType: 'standard', // 'standard', 'vue', 'react'
-  env: {
+  'bundlerType': 'standard', // 'standard', 'vue', 'react'
+  'dts-gen': {
+    env: 'development',
+  },
+  'env': {
     // TODO: dts-gen, ginue, プログラム内で全て共通化
     development: {
       name: 'development',
       host: 'the-red.cybozu.com',
       app: {
-        project: 148,
-        customer: 149,
-        sales: 150,
+        'project': 148,
+        'customer': 149,
+        'sales-activity': 150,
       },
     },
     staging: {
       name: 'staging',
       host: 'the-red.cybozu.com',
       app: {
-        project: 169,
-        customer: 170,
-        sales: 171,
+        'project': 169,
+        'customer': 170,
+        'sales-activity': 171,
       },
       guest: 34,
     },
@@ -25,9 +28,9 @@ module.exports = {
       name: 'production',
       host: 'the-red.cybozu.com',
       app: {
-        project: 175,
-        customer: 176,
-        sales: 177,
+        'project': 175,
+        'customer': 176,
+        'sales-activity': 177,
       },
     },
   },
