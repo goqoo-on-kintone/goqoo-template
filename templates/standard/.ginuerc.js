55cf15a4745f31e15c9c48a972e00b5d38656551
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-08-12 14:41:11 +0900

    refactor: 変数名修正

--- templates/standard/.ginuerc.js
+++ templates/standard/.ginuerc.js
@@ -6,5 +6,5 @@ module.exports = {
   location: 'kintone-settings',
   fileType: 'js',
   alt: true,
-  env: Object.fromEntries(config.environments.map((_) => [_.env, { app: _.appId, ..._ }])),
+  env: Object.fromEntries(config.environments.map((obj) => [obj.env, { app: obj.appId, ...obj }])),
 }
