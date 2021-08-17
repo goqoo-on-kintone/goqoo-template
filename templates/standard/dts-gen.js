c12fb72a27512fb49d28a4eac0afe6b7840e22f0
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-08-17 13:49:09 +0900

    refactor: dts-gen関連は削除

--- templates/standard/dts-gen.js
+++
@@ -1,5 +0,0 @@
-require('ts-node').register({ typeCheck: false, transpileOnly: true })
-const { dts } = require('../../../goqoo/src/generator/dts/index.ts')
-const { default: config } = require('./goqoo.config.ts')
-
-dts(config)
