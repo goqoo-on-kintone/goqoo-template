d64e824489524df93c7725881eeb1f20bb671d19
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-08-19 22:32:40 +0900

    feat: レコード一括保存を実装(Vue)

--- templates/vue/src/in-app/customize-view/[_name_].ts
+++ templates/vue/src/in-app/customize-view/[_name_].ts
@@ -1,5 +1,5 @@
 import { KintoneRestAPIClient } from '@kintone/rest-api-client'
-import { helloGoqoo, confirmDialog, successDialog } from 'swal-customize'
+import { helloGoqoo, confirmDialog, successDialog, errorDialog } from 'swal-customize'
 import type { IndexEvent } from 'types'
 
 import Vue from 'vue'
@@ -21,9 +21,11 @@ kintone.events.on('app.record.index.show', async (event: IndexEvent<any /* kinto
 
   const fieldCodes = /*%& fieldCodes %*/ /*% */ ['案件名', 'プラン費用', 'オプション費用', '詳細'] /* %*/
   const fields = fieldCodes.map((code) => ({ code, label: properties[code].label }))
-  const records = event.records.map((record) =>
-    Object.fromEntries(fieldCodes.map((code) => [code, record[code]]))
-  )
+
+  const records = await client.record.getAllRecords({
+    app: kintone.app.getId()!,
+    condition: kintone.app.getQueryCondition()!,
+  })
 
   divNode.insertAdjacentHTML('beforeend', HTML_TEMPLATE)
   new Vue({
@@ -35,14 +37,29 @@ kintone.events.on('app.record.index.show', async (event: IndexEvent<any /* kinto
       },
     },
     methods: {
-      async saveAllRecords() {
-        if (!(await confirmDialog(`${this.records.length}件のレコードを保存します。よろしいですか？`))) return
-        await new Promise((resolve) => setTimeout(resolve, 3000))
-        await successDialog('完了しました。')
-        location.reload()
-      },
       async hello() {
         await helloGoqoo()
+      },
+      async saveAllRecords() {
+        try {
+          if (!(await confirmDialog(`${this.records.length}件のレコードを保存します。よろしいですか？`))) {
+            return
+          }
+
+          await client.record.updateAllRecords({
+            app: kintone.app.getId() as number,
+            records: this.records.map((record) => ({
+              id: record.$id.value as string,
+              revision: record.$revision.value as string,
+              record: Object.fromEntries(fieldCodes.map((code) => [code, { value: record[code].value }])),
+            })),
+          })
+
+          await successDialog('完了しました。')
+          location.reload()
+        } catch (e) {
+          errorDialog(e)
+        }
       },
     },
   })
