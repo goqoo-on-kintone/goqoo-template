d681ff899508a666b302ac3e969c232791594279
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-08-20 01:33:01 +0900

    fix: image.d.tsは削除

--- templates/vue/dts/image.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.png'
-declare module '*.jpg'
-declare module '*.gif'
-declare module '*.svg'
-declare module '*.eot'
-declare module '*.ttf'
-declare module '*.woff'
-declare module '*.woff2'
