f9c92778a3be82ba897250602a1f467cfbe79043
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-12-15 00:46:44 +0900

    feat: eslint-config-goqoo導入

--- templates/default/src/apps/app/hello.ts
+++ templates/default/src/apps/app/hello.ts
@@ -1,7 +1,7 @@
-import { helloGoqoo } from 'goqoo'
 import type { IndexEvent } from 'types'
+import { helloGoqoo } from 'goqoo'
 
-kintone.events.on('app.record.index.show', async (event: IndexEvent<any /* kintone.types.SavedXxxxFields */>) => {
+kintone.events.on('app.record.index.show', async (event: IndexEvent<any /* Kintone.types.SavedXxxxFields */>) => {
   await helloGoqoo()
   return event
 })
