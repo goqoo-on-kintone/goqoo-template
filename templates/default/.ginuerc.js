c0274a50da2a28138ab49ca6ab5edb03174fd343
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-12-18 07:22:02 +0900

    chore: Uninstall ginue

--- templates/default/.ginuerc.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const config = require('./goqoo.config')
-
-module.exports = {
-  location: 'kintone-settings',
-  fileType: 'js',
-  alt: true,
-  env: Object.fromEntries(config.environments.map((obj) => [obj.env, { app: obj.appId, ...obj }])),
-}
