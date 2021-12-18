068756de42e1f804eac5f7f47e7cb2d8d8a6d1ff
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-12-19 00:20:18 +0900

    feat: reactのファイルをほぼシンボリックリンクに

--- templates/react/goqoo.config.js
+++ templates/react/goqoo.config.js
@@ -1,22 +1,42 @@
 // @ts-check
 
 /**
- * @typedef {'development' | 'staging' | 'production'} Env
- *
- * @typedef {{
- *  'app': number
- * }} AppId
- *
- * @typedef {{
- *  'app'?: string
- * }} ApiToken
- */
-
-/**
- * @type {import('goqoo').Config<Env, AppId, ApiToken>}
+ * @type {import('./goqoo.config.types').Config}
  */
 const config = {
   bundlerType: 'react',
-  environments: [],
+  dtsGen: {
+    env: 'development',
+    // skip: ['customer'],
+  },
+  environments: [
+    {
+      env: 'development',
+      host: 'the-red.cybozu.com',
+      appId: {
+        project: 148,
+        customer: 149,
+        sales_activity: 150,
+      },
+    },
+    {
+      env: 'staging',
+      host: 'the-red.cybozu.com',
+      appId: {
+        project: 169,
+        customer: 170,
+        sales_activity: 171,
+      },
+    },
+    {
+      env: 'production',
+      host: 'the-red.cybozu.com',
+      appId: {
+        project: 175,
+        customer: 176,
+        sales_activity: 177,
+      },
+    },
+  ],
 }
 module.exports = config
